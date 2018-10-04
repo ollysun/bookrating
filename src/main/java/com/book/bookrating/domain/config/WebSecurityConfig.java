29dfa59277050966df3fa2f661503b1b7d0005ee
ollysun <ollysun@gmail.com>
2018-10-04 15:04:53 +0100

    swagger configuration

--- src/main/java/com/book/bookrating/domain/config/WebSecurityConfig.java
+++ src/main/java/com/book/bookrating/domain/config/WebSecurityConfig.java
@@ -49,7 +49,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                .antMatchers("/token/*", "/signup").permitAll()
+                .antMatchers(
+                        HttpMethod.GET,
+                        "/",
+                        "/swagger-ui.html",
+                        "/configuration/**",
+                        "/*.html",
+                        "/v2/api-docs",
+                        "/webjars/**",
+                        "/swagger-resources/**",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/token/*",
+                        "/signup").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
--- src/main/java/com/book/bookrating/swagger/SwaggerConfiguration.java
+++ src/main/java/com/book/bookrating/swagger/SwaggerConfiguration.java
@@ -3,11 +3,17 @@ package com.book.bookrating.swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
@@ -19,6 +25,22 @@ public class SwaggerConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(appInfo())
+                .securitySchemes(Arrays.asList(apiKey()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("authkey", "Authorization", "header");
+    }
+
+    private ApiInfo appInfo() {
+        return new ApiInfoBuilder().title("REST API")
+                .description("The rest api demo for book rating").termsOfServiceUrl("")
+                .contact(new Contact("Moses Olalere", "", "ollysun@gmail.com"))
+                .license("Apache License Version 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .version("0.0.1")
                 .build();
     }
 }
