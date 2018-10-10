762e9a547f5cf4238fcf401e19a54452546e0089
ollysun <ollysun@gmail.com>
2018-10-10 16:57:21 +0100

    add book repo and book postgree dump

---
+++ src/test/java/com/book/bookrating/bookRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.book.bookrating;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
+public class bookRepositoryTest {
+}
